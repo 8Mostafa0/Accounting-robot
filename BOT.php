e28bbb9e24decb1738245fe53d86595834aa8bb9
unknown <mosielite@gmail.com>
2024-03-14 17:39:02 +0330

    add bot file and make first parts in code

---
+++ BOT.php
@@ -0,0 +1,33 @@
+<?PHP
+$bot_token = "";
+$url = "https://api.telegram.org/bot".$bot_token;
+
+$json_updates = file_get_contents("php://input");
+$updates = json_decode($json_updates, true);
+
+if(isset($updates['message'])){
+    $chat_id = $updates['message']['chat']['id'];
+    $text = $updates['message']['text'];
+    send_message($text);
+}
+
+//!========= > MAIN FUNCTIONS
+    //!=========== SEND MESSAGE
+    function send_message($text){
+        $url = $GLOBALS['url']."/sendMessage";
+        $parameters = ['chat_id'=>$GLOBALS['chat_id'],'text'=>$text];
+        $result = send_request($url,$parameters);
+        return $result;
+    }
+    //!========== REQUEST FUNCTION
+    function send_request($url,$parameters){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); 
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+?>
